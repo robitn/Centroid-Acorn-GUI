752e31b1c2239d76ae53d813140c3adf8ea92f93
robitn <tim.n.robinson@gmail.com>
2026-02-06 14:31:35 -0600

    Scaffold Avalonia app with Ab4d.SharpEngine TVP

--- obj/Debug/net8.0/Centroid-Acorn-GUI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Centroid-Acorn-GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Centroid-Acorn-GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af94cbca1919f546a236d8df75bff791e9cedd2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Centroid-Acorn-GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Centroid-Acorn-GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
